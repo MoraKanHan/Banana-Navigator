2c782bd6c0773f7f2027db9128082a86241e31fd
MoraKanHan <marwa.kandil@live.com>
2021-09-03 16:33:24 +0300

    Add files via upload

---
+++ Double DQN/Navigation_DoubleDQN.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Navigation
+# 
+# ---
+# 
+# In this notebook, you will learn how to use the Unity ML-Agents environment for the first project of the [Deep Reinforcement Learning Nanodegree](https://www.udacity.com/course/deep-reinforcement-learning-nanodegree--nd893).
+# 
+# ### 1. Start the Environment
+# 
+# We begin by importing some necessary packages.  If the code cell below returns an error, please revisit the project instructions to double-check that you have installed [Unity ML-Agents](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Installation.md) and [NumPy](http://www.numpy.org/).
+
+# In[1]:
+
+
+
+from unityagents import UnityEnvironment
+from collections import namedtuple, deque
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import matplotlib.pyplot as plt
+
+
+# Next, we will start the environment!  **_Before running the code cell below_**, change the `file_name` parameter to match the location of the Unity environment that you downloaded.
+# 
+# - **Mac**: `"path/to/Banana.app"`
+# - **Windows** (x86): `"path/to/Banana_Windows_x86/Banana.exe"`
+# - **Windows** (x86_64): `"path/to/Banana_Windows_x86_64/Banana.exe"`
+# - **Linux** (x86): `"path/to/Banana_Linux/Banana.x86"`
+# - **Linux** (x86_64): `"path/to/Banana_Linux/Banana.x86_64"`
+# - **Linux** (x86, headless): `"path/to/Banana_Linux_NoVis/Banana.x86"`
+# - **Linux** (x86_64, headless): `"path/to/Banana_Linux_NoVis/Banana.x86_64"`
+# 
+# For instance, if you are using a Mac, then you downloaded `Banana.app`.  If this file is in the same folder as the notebook, then the line below should appear as follows:
+# ```
+# env = UnityEnvironment(file_name="Banana.app")
+# ```
+
+# In[2]:
+
+
+env = UnityEnvironment(file_name="/home/deeprl/deep-reinforcement-learning/p1_navigation/Banana_Linux/Banana.x86_64", seed=1)
+env.reset()
+
+
+# Environments contain **_brains_** which are responsible for deciding the actions of their associated agents. Here we check for the first brain available, and set it as the default brain we will be controlling from Python.
+
+# In[3]:
+
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+print(brain)
+
+
+# ### 2. Examine the State and Action Spaces
+# 
+# The simulation contains a single agent that navigates a large environment.  At each time step, it has four actions at its disposal:
+# - `0` - walk forward 
+# - `1` - walk backward
+# - `2` - turn left
+# - `3` - turn right
+# 
+# The state space has `37` dimensions and contains the agent's velocity, along with ray-based perception of objects around agent's forward direction.  A reward of `+1` is provided for collecting a yellow banana, and a reward of `-1` is provided for collecting a blue banana. 
+# 
+# Run the code cell below to print some information about the environment.
+
+# In[4]:
+
+
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space 
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+
+# ### 3. Create QNetwork class!
+# 
+# This class conatians details of the Deep Neural Network
+
+# In[5]:
+
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, hidden_layers=[32,64,128,256],drop_p=0.3):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        # Add the first layer, input to a hidden layer
+        self.hidden_layers = nn.ModuleList([nn.Linear(state_size, hidden_layers[0])])
+        
+        # Add a variable number of more hidden layers
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+        
+        self.dropout = nn.Dropout(p=drop_p) #dropout layer
+        
+        self.output = nn.Linear(hidden_layers[-1], action_size) #output layer
+        
+       
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+         # Forward through each layer in `hidden_layers`, with ReLU activation and dropout
+        x = state 
+        for linear in self.hidden_layers:
+            x = F.relu(linear(x))
+        
+        x = self.dropout(x)
+        x = self.output(x)
+        return x
+
+
+# ### 5. Create Agent class!
+# 
+# This class conatians details of the Double DQN agent
+
+# In[6]:
+
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-4               # learning rate 
+UPDATE_EVERY = 4        # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class DDQNAgent():
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize an Agent object.
+        
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+
+        # Q-Network
+        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)#to reduce fluctuation in q values
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
+    
+    def step(self, state, action, reward, next_state, done):
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+        
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
+
+    def act(self, state, eps=0.):
+        """Returns actions for given state as per current policy.
+        
+        Params
+        ======
+            state (array_like): current state
+            eps (float): epsilon, for epsilon-greedy action selection
+        """
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+
+    def learn(self, experiences, gamma):
+        """Update value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        ## TODO: compute and minimize the loss
+        # STEP 1: get Target Q values
+        max_actions = self.qnetwork_target(next_states).detach().max(1)[1].unsqueeze(1)#selecting best action
+        Q_targets_next = self.qnetwork_target(next_states).gather(1,max_actions)# Get max predicted Q values (for next states) from target model
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))# Compute Q targets for current states 
+        
+        # STEP 2:Get expected Q values from local model
+        Q_expected = self.qnetwork_local(states).gather(1,actions)
+        
+        #STEP 3:calculating loss
+        loss = F.mse_loss(Q_expected , Q_targets)
+        
+        #STEP 4:Minimizing loss
+        self.optimizer.zero_grad() # resetting gradients
+        loss.backward() #finding derivative of loss
+        self.optimizer.step()
+        
+        # ------------------- update target network ------------------- #
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
+
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model (PyTorch model): weights will be copied from
+            target_model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter 
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+
+# ### 6. Create ReplayBuffer class!
+# 
+# This class conatians details of the Replay buffer class
+
+# In[7]:
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+
+        Params
+        ======
+            action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)  
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+  
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+# ### 7. Train the Agent with DDQN!
+# 
+# Run the code cell below to train the agent from scratch.  You are welcome to amend the supplied values of the parameters in the function, to try to see if you can get better performance!
+
+# In[8]:
+
+
+env_info = env.reset(train_mode=True)[brain_name]
+agent = DDQNAgent(state_size=state_size, action_size=action_size, seed=0)
+
+def ddqn(n_episodes=1500, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] #reset the environment with every episode
+        state = env_info.vector_observations[0]   
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name] #Taking one step
+            next_state = env_info.vector_observations[0]   
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>= 13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint2.pth')
+            break
+    return scores
+
+scores = ddqn()
+
+
+# ### 8. plot the scores!
+# 
+# Run the code cell below to plot the number of episodes aganist the scores.
+
+# In[9]:
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+env.close()
+        
+
+
+# ### 9. Watch a Smart Agent!
+# 
+# load the trained weights from file to watch a smart agent!
+
+# In[6]:
+
+
+agent = QNetwork(state_size=state_size, action_size=action_size, seed=0)
+agent.load_state_dict(torch.load('checkpoint2.pth'))
+
+def act(state):
+        """Returns actions for given state as per current policy.
+        
+        Params
+        ======
+            state (array_like): current state
+        """
+        state = torch.from_numpy(state).float().unsqueeze(0).to("cpu")
+        agent.eval()
+        with torch.no_grad():
+            action_values = agent(state)
+        
+        return np.argmax(action_values.cpu().data.numpy())
+
+env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+state = env_info.vector_observations[0]            # get the current state
+score = 0                                          # initialize the score
+while True:
+    action = act(state).astype(int)                # select an action
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if done:                                       # exit loop if episode finished
+        break
+    
+print("Score: {}".format(score))           
+            
+            
+env.close()
+
